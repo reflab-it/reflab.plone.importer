bef3a8477141ede6588f0902cad7388935b9be16
micheg <michelangelog@gmail.com>
2022-03-16 17:33:54 +0000

    deserializer

---
+++ src/reflab/plone/importer/deserializers/integer.py
@@ -0,0 +1,6 @@
+def deserialize(value):
+    try:
+        tmp = int(value)
+    except:
+        tmp = 0
+    return tmp
---
+++ src/reflab/plone/importer/deserializers/list.py
@@ -0,0 +1,5 @@
+def deserialize(value):
+    #print("deserialize list, value is => " + str(value))
+    if value is None:
+        return []
+    return value
