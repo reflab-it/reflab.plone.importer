2df0d06ecbf984da930c5455905aaf50c4326137
Francesco Merlo <francesco.merlo@reflab.it>
2022-10-18 15:44:30 +0200

    On create task check if the container is really a folderish content

--- src/reflab/plone/importer/tasks/create.py
+++ src/reflab/plone/importer/tasks/create.py
@@ -1,18 +1,15 @@
 from  plone import api
 from plone.uuid.interfaces import ATTRIBUTE_NAME as UID_ATTRIBUTE_NAME
-CREATE_GLOBALS = dict(
-    limit = 0, # edit here for debug purposes
-    counter = 0
-)
+from Products.CMFCore.interfaces import IFolderish
+
 
 def task(importer, container, data):
-    if CREATE_GLOBALS['limit'] > 0:
-        if CREATE_GLOBALS['counter'] > CREATE_GLOBALS['limit']:
-            return 
-
-    available_types = api.portal.get_tool('portal_types').objectIds()
-
     id = data['id']
+    
+    if not IFolderish.providedBy(container):
+        importer.logger.error(f"Item in {container.absolute_url()} is not a container; '{id} not created'")
+        return
+
     if id in container.objectIds():
         importer.logger.warning(
             f"Already exists {id} in {'/'.join(container.getPhysicalPath())}"
@@ -20,16 +17,16 @@ def task(importer, container, data):
         return
 
     attributes = data['fields']
-    
     for invalid_name in ['id', 'type', 'container']:
         if invalid_name in attributes.keys():
-            importer.logger.warning(
+            importer.logger.debug(
                 f"A field with name '{invalid_name}' can't be used during the\
                 creation of a '{data['portal_type']}' and it will be ignored."
             )
             del(attributes[invalid_name])
 
     portal_type = data['portal_type']
+    available_types = api.portal.get_tool('portal_types').objectIds()
     if portal_type not in available_types:
         importer.logger.warning(f'The portal type {portal_type} is not availabe, it will be ignored')
         return
@@ -61,9 +58,6 @@ def task(importer, container, data):
                 obj._setProperty(property_name, property_value, 'boolean')                
             else:
                 importer.logger.warning(f'Unsupported property type {property_value_type}')
-
-
-    CREATE_GLOBALS['counter'] += 1
 
     importer.logger.info(
         f"Created {id} in {'/'.join(container.getPhysicalPath())}"
